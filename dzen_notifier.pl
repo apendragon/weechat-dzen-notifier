fa899d711666d579fb076d7542ab52a82ae75856
Thomas Cazali <tcazali@gmail.com>
2015-03-15 18:34:01 +0100

    unnotify works

--- dzen_notifier.pl
+++ dzen_notifier.pl
@@ -12,14 +12,20 @@ use constant {
   _DEBUG       => 1,
 };
 
+my %options = (
+  icon     => '^i(/home/tcazali/.dzen/icons/xbm8x8/cat.xbm)',
+  dzen_cmd => "dzen2 -ta l -h 18 -fn 'snap' -bg '#111111' -fg '#b3b3b3' -w 200 -x 1000",
+);
+
 weechat::register(_SCRIPT_NAME(), _AUTHOR(), _VERSION(), _LICENSE(), _DESC(), _SHUTDOWN_F(), _CHARSET());
 
-my $dzen_bar = "dzen2 -ta l -h 18 -fn 'snap' -bg '#111111' -fg '#b3b3b3' -w 200 -x 1000";
-open my ($io_fh), "|$dzen_bar";
+open my ($io_fh), "|$options{dzen_cmd}";
 $io_fh->autoflush(1);
-my %msg_stack= ();
+my %buffered_pv_msg = ();
+my @stacked_notif = ();
 
 weechat::hook_print('', '', '', 1, 'print_author_and_count_priv_msg', '');
+weechat::hook_signal('buffer_switch', 'unnotify', '');
 
 sub get_msg_sender {
   my ($tags) = @_;
@@ -44,27 +50,30 @@ sub is_private_message {
 }
 
 sub notify {
-  my ($buffer, $tags) = @_;
+  my ($sender) = @_;
   weechat::log_print("notify") if _DEBUG();
-  my $sender = get_msg_sender($tags);
-  $msg_stack{$sender}++;
-  my $count = scalar keys %msg_stack;
-  print $io_fh "[$count] $sender ($msg_stack{$sender})\n";
+  my $count = scalar keys %buffered_pv_msg;
+  $count ? print $io_fh "$options{icon} [$count] $sender ($buffered_pv_msg{$sender})\n" : print $io_fh "\n";
   weechat::WEECHAT_RC_OK;
-  #TODO think about stack order
 }
 
 sub notify_on_private {
   my ($buffer, $tags) = @_;
   weechat::log_print("notify_on_private") if _DEBUG();
-  is_private_message($buffer, $tags) ? notify($buffer, $tags) : weechat::WEECHAT_RC_OK;
+  if (is_private_message($buffer, $tags)) {
+    my $sender = get_msg_sender($tags);
+    rm_from_stack($sender);
+    push(@stacked_notif, $sender);
+    $buffered_pv_msg{$sender}++;
+    notify $sender;
+  } else {
+    weechat::WEECHAT_RC_OK;
+  }
 }
 
 sub print_author_and_count_priv_msg {
   my ($data, $buffer, $date, $tags, $displayed, $highlight, $prefix, $message) = @_;
   weechat::log_print("print_author_and_count_priv_msg") if _DEBUG();
-  weechat::log_print("tags:$tags");
-  weechat::log_print("buffer:$buffer");
   my $dispatch = {
     0 => sub { weechat::WEECHAT_RC_OK }, # return if message is filtered
     1 => sub {
@@ -72,6 +81,33 @@ sub print_author_and_count_priv_msg {
     },
   };
   $dispatch->{$displayed}->();
+}
+
+sub unnotify {
+  my ($signal, $type_data, $signal_data) = @_;
+  weechat::log_print("unnotify") if _DEBUG();
+  my $type=weechat::buffer_get_string($signal_data, 'localvar_type');
+  my $channel=weechat::buffer_get_string($signal_data, 'localvar_channel');
+  my $dispatch = {
+    'private' => sub {
+       rm_sender_notif($channel);
+       scalar(@stacked_notif) ? notify($stacked_notif[-1]) : notify();
+     },
+  };
+  ($dispatch->{$type} || sub { weechat::WEECHAT_RC_OK })->();
+}
+
+sub rm_from_stack {
+  my ($sender) = @_;
+  my @stack = ();
+  foreach (@stacked_notif) { push(@stack, $_) if ($_ ne $sender) }
+  @stacked_notif = @stack;
+}
+
+sub rm_sender_notif {
+  my ($sender) = @_;
+  delete($buffered_pv_msg{$sender});
+  rm_from_stack($sender);
 }
 
 sub shutdown {
