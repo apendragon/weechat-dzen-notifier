c666c21f053da88ec5304ee445eaa6c2a0ef9e4c
Thomas Cazali <tcazali@gmail.com>
2015-03-28 17:07:20 +0100

    add AUTHOR, SOURCE, BUGS, SUPPORT, etc to POD

--- dzen_notifier.pl
+++ dzen_notifier.pl
@@ -101,6 +101,72 @@ C<[4] johndoe (2)>
 it means you have C<4> private message buffers you have not already read, the last private message you received is in C<johndoe> buffer,
 and he wrote C<2> unread messages in this buffer.
 
+=head1 AUTHOR
+
+Thomas Cazali
+
+=head1 SOURCE
+
+The source code repository for C<dzen_notifier.pl> can be found at L<https://github.com/apendragon/weechat-dzen-notifier>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to L<https://github.com/apendragon/weechat-dzen-notifier/issues>.
+
+=head1 SUPPORT
+
+You can find documentation for this script at
+
+L<https://github.com/apendragon/weechat-dzen-notifier/wiki>
+
+=over 4
+
+=item * github repository issues tracker (report bugs here)
+
+L<https://github.com/apendragon/weechat-dzen-notifier/issues>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Thomas Cazali.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 =cut
 
 open my ($io_fh), "|$options{dzen_cmd}";
